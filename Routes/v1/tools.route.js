e1930f2a4f57612b2644997bfde38eff3804622c
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-10-23 19:12:47 +0600

    add api comment

--- Routes/v1/tools.route.js
+++ Routes/v1/tools.route.js
@@ -34,10 +34,40 @@ router.route("/").get((req, res) => {
 */
 
 router.route("/")
+    /**
+     * @api {get} /tools All tools
+     * @apiDescription Get all the tools
+     * @apiPermission admin
+     *
+     * @apiHeader {String} Authorization   User's access token
+     *
+     * @apiParam  {Number{1-}}         [page=1]     List page
+     * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+     *
+     * @apiSuccess {Object[]} all the tools.
+     *
+     * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+     * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+     */
 
-.get(ToolsController.getAllProducts)
+    .get(ToolsController.getAllProducts)
+    /**
+     * @api {post} /tools save a tool
+     * @apiDescription Get all the tools
+     * @apiPermission admin
+     *
+     * @apiHeader {String} Authorization   User's access token
+     *
+     * @apiParam  {Number{1-}}         [page=1]     List page
+     * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+     *
+     * @apiSuccess {Object[]} all the tools.
+     *
+     * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+     * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+     */
 
-.post(ToolsController.SaveAProducts)
+    .post(ToolsController.SaveAProducts)
 
 
 module.exports = router;
