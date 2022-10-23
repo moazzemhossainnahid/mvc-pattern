58ff86b9d69f2776330cfddbc7ad103638d3ca8c
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-10-23 11:39:12 +0600

    export import route

---
+++ Routes/tools.route.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+router.get("/", (req, res) => {
+    res.send("Tools Found");
+})
+
+
+router.get("/:id", (req, res) => {
+    res.send("Tools Found by ID");
+})
+
+
+router.post("/products", (req, res) => {
+    res.send("Tool Added");
+})
+
+
+module.exports = router;
